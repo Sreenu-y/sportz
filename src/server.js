8e1d299d261e3776f9c283aa6217d331e0724a65
Sreenu-y <srinuyelesam123@gmail.com>
2026-09-09 17:59:55 +0530

    implement rate limit security

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import { matchRouter } from "./routes/matches.js";
 import { attachWebSocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./arcjet.js";
 
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || "0.0.0.0";
@@ -14,6 +15,8 @@ app.get("/", (_, res) => {
 });
 
 app.use(express.json());
+
+app.use(securityMiddleware());
 
 app.use("/matches", matchRouter);
 
